f68d376d8363e1d8931e74dcd636fe4e429228d7
Vikar Maulana Arrisyad <vikar.maulana.arrisyad@gmail.com>
2022-09-10 18:54:43 +0700

    first app

---
+++ database/migrations/2022_09_10_112905_create_category_projects_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('category_projects', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('category_id');
+            $table->unsignedBigInteger('project_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('category_projects');
+    }
+};
---
+++ list.md
@@ -0,0 +1,27 @@
+## membuat migration
+    - roles
+    - users
+    - categories
+    - project
+    - post
+
+## membuat autentikasi dan otorisasi
+    - jetstream
+
+## Integrasi Template 
+    - Template Stisla (backend)
+    - (Frontend)
+
+## Manajement Category
+    - CRUD Modal 
+    - Sweetalert2
+    - datatables server side
+
+## Manajement Project
+    - CRUD Modal
+    - Sweetalert
+    - datatables server side
+    - filter data
+
+## 
+
